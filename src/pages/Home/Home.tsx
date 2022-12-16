36387f556cd4c59c48523043f97facb278e227c6
efdalyalcin <efdalyalcin@hotmail.com>
2022-12-16 17:52:04 +0300

    store connection, flatlist introduced

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,27 @@
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, ScrollView, FlatList } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { styles } from './Home.style';
-import { Button } from 'react-native-paper';
+import { Button, Card, Paragraph, Title } from 'react-native-paper';
 import BottomNav from '../../components/BottomNav/BottomNav';
 import { HeaderStyle } from '../../styles/Header';
-import { getProducts } from 'src/services/getProducts';
+import { getProducts, IProduct } from 'src/services/getProducts';
 
-const height = Dimensions.get('screen').height - HeaderStyle.headerSize;
+const height = Dimensions.get('screen').height - HeaderStyle.headerSize - 80;
+
+const renderItem = ({ item }) => {
+  return (
+    <Card>
+      <Card.Title title={item.title} />
+      <Card.Content>
+        <Paragraph>{item.description}</Paragraph>
+      </Card.Content>
+      <Card.Cover source={{ uri: item.image }} resizeMode="contain" />
+      <Card.Actions>
+        <Button>Add to Cart</Button>
+      </Card.Actions>
+    </Card>
+  );
+};
 
 export default function Home({
   navigation,
@@ -18,17 +33,20 @@ export default function Home({
   const [shopList, setShopList] = useState<any>();
 
   useEffect(() => {
-    const data = getProducts();
-    setShopList(data.data);
+    getProducts().then((res) => setShopList(res));
   }, []);
+
   console.log(shopList);
   return (
-    <View style={(styles.container, { height: height })}>
-      {/* <Text>Home</Text>
-      <Button mode="contained" onPress={() => console.log('Pressed')}>
-        Go to the Chart
-      </Button> */}
+    <>
+      <View style={(styles.container, { height: height })}>
+        <FlatList
+          renderItem={renderItem}
+          data={shopList}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
       <BottomNav />
-    </View>
+    </>
   );
 }
--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -1,31 +1,30 @@
 import axios from 'axios';
 export const BASE_URL = 'https://fakestoreapi.com';
 
-const options = {
-  method: 'GET',
-  url: `${BASE_URL}/products`,
+export interface IProductRate {
+  count: number;
+  rate: number;
+}
+export interface IProduct {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: IProductRate;
+  title: string;
+}
+
+export const getProducts = () => {
+  return new Promise<IProduct>((resolve, reject) => {
+    axios
+      .get(`${BASE_URL}/products`)
+      .then((res) => {
+        // console.log('the data from the server ===> ', res.data);
+        resolve(res.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 };
-
-export const getProducts = async () => {
-  const data = await axios
-    .request(options)
-    .then((res) => res)
-    .catch((error) => {
-      console.error(error);
-    });
-
-  return data;
-};
-
-// export const getPlayers = async () => {
-//   const data = await axios
-//     .request(options)
-//     .then((res) => {
-//       return res;
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-
-//   return data;
-// };

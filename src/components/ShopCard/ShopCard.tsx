9ab3913ce953c9ea80bb882a8ece9df7606420f9
efdalyalcin <efdalyalcin@hotmail.com>
2023-01-02 15:12:57 +0300

    favorites button created

--- src/components/ShopCard/ShopCard.tsx
+++ src/components/ShopCard/ShopCard.tsx
@@ -2,23 +2,51 @@ import { View, Text, Image, Pressable } from 'react-native';
 import React from 'react';
 import { styles } from './ShopCard.style';
 import { Shadow } from 'react-native-shadow-2';
+// @ts-ignore
 import FavoriteIcon from 'assets/icons/favorite_filled.svg';
+// @ts-ignore
+import NotFavoriteIcon from 'assets/icons/favorite_outlined.svg';
 import { Svg } from 'react-native-svg';
+import { IProduct } from 'src/shared/productInterface';
+import useFavoriteProducts from 'src/store/favoriteProducts';
 
 type Props = {
   title: string;
   description: string;
   pic: string;
+  item: IProduct;
 };
 
-const ShopCard = ({ title, description, pic }: Props) => {
+const ShopCard = ({ title, description, pic, item }: Props) => {
+  const { favorites, addToFavorites, removeFromFavorites } =
+    useFavoriteProducts();
+
+  const inFavorites = favorites.find((favItem) => favItem?.id === item.id);
+
   return (
     <View style={{ padding: 10 }}>
       <Shadow>
         <View style={[styles.card, styles.shadowProp]}>
-          <Pressable style={styles.favorite}>
-            <FavoriteIcon width={200} height={200} />
-          </Pressable>
+          {inFavorites ? (
+            <Pressable
+              style={styles.favorite}
+              onPress={() => removeFromFavorites(item)}
+            >
+              <FavoriteIcon
+                width={24}
+                height={24}
+                fill={'red'}
+                style={styles.favIcon}
+              />
+            </Pressable>
+          ) : (
+            <Pressable
+              style={styles.favorite}
+              onPress={() => addToFavorites(item)}
+            >
+              <NotFavoriteIcon width={24} height={24} fill={'red'} />
+            </Pressable>
+          )}
           <Text style={styles.title}>{title}</Text>
           <Text style={styles.desc} numberOfLines={3} ellipsizeMode="tail">
             {description}

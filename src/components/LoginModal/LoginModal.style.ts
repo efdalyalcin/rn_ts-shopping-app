4052b23285f00ed18825b412000a8afdc71c784e
efdalyalcin <efdalyalcin@hotmail.com>
2023-02-18 15:25:26 +0300

    login page layout

--- src/components/LoginModal/LoginModal.style.ts
+++ src/components/LoginModal/LoginModal.style.ts
@@ -1,13 +1,44 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+import { colorStyles } from 'src/styles/colors';
+
+const height = Dimensions.get('screen').height;
 
 export const styles = StyleSheet.create({
+  container: {
+    marginTop: 64,
+    height: height - 64,
+    width: '100%',
+    backgroundColor: colorStyles.background,
+  },
   closeButton: {
     display: 'flex',
     alignItems: 'flex-end',
+    marginRight: 10,
+    padding: 4,
   },
-  email: {
+  inputs: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputBlock: {
+    width: '70%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  textInput: {
+    marginLeft: 8,
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: 'red',
+    backgroundColor: colorStyles.primary,
+    borderRadius: 16,
+    flex: 1,
+  },
+  label: {
+    paddingHorizontal: 20,
+    paddingVertical: 4,
   },
 });
--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -3,19 +3,20 @@ import {
   Text,
   TextInput,
   Pressable,
-  Dimensions,
   Modal,
   Alert,
+  Animated,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Formik } from 'formik';
 import { styles } from './LoginModal.style';
-import { Button } from 'react-native-paper';
 // @ts-ignore
 import CloseIcon from 'assets/icons/close-circle-outline.svg';
+// @ts-ignore
+import AccountIcon from 'assets/icons/account_filled.svg';
+// @ts-ignore
+import PasswordIcon from 'assets/icons/password_filled.svg';
 import { colorStyles } from 'src/styles/colors';
-
-const height = Dimensions.get('screen').height;
 
 type Props = {
   isModalVisible: boolean;
@@ -26,6 +27,31 @@ export default function LoginModal({
   isModalVisible,
   setIsModalVisible,
 }: Props) {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      delay: 500,
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    const fadeing = setTimeout(() => {
+      fadeIn();
+    }, 300);
+  }, []);
+
   return (
     <Modal
       animationType="slide"
@@ -37,33 +63,47 @@ export default function LoginModal({
       }}
     >
       <Formik
-        initialValues={{ email: '' }}
+        initialValues={{ username: '', password: '' }}
         onSubmit={(values) => console.log(values)}
       >
         {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <View
-            style={{
-              marginTop: 64,
-              height: height - 64,
-              width: '100%',
-              backgroundColor: 'orange',
-            }}
-          >
+          <View style={styles.container}>
             <Pressable
               // icon="close-circle-outline"
-              onPress={() => setIsModalVisible(false)}
+              onPress={() => {
+                setIsModalVisible(false);
+                fadeOut();
+              }}
               style={styles.closeButton}
             >
-              <CloseIcon width={48} height={48} fill={colorStyles.primary} />
+              <CloseIcon width={24} height={24} fill={colorStyles.main} />
             </Pressable>
-            <TextInput
-              onChangeText={handleChange('email')}
-              onBlur={handleBlur('email')}
-              value={values.email}
-              style={styles.email}
-              placeholder="email@email.com"
-            />
-            {/* <Pressable onPress={handleSubmit} title="Submit" /> */}
+            <Animated.View style={[{ opacity: fadeAnim }]}>
+              <Text>Log In</Text>
+            </Animated.View>
+            <View style={styles.inputs}>
+              <View style={styles.inputBlock}>
+                <AccountIcon fill={colorStyles.text} width={24} height={24} />
+                <TextInput
+                  onChangeText={handleChange('username')}
+                  onBlur={handleBlur('username')}
+                  value={values.username}
+                  style={styles.textInput}
+                  placeholder="username"
+                />
+              </View>
+              <View style={styles.inputBlock}>
+                <PasswordIcon fill={colorStyles.text} width={24} height={24} />
+                <TextInput
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  style={styles.textInput}
+                  placeholder="password"
+                />
+              </View>
+              {/* <Pressable onPress={handleSubmit} title="Submit" /> */}
+            </View>
           </View>
         )}
       </Formik>
